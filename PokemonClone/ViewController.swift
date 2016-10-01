846118edd177c07705632d4f5ff0cf88b4ff1b68
Diane Hoffstetter <dhoffstetter@mac.com>
2016-09-30 21:46:01 -0700

    Random pins.

--- PokemonClone/ViewController.swift
+++ PokemonClone/ViewController.swift
@@ -7,12 +7,63 @@
 //
 
 import UIKit
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+  @IBOutlet weak var mapView: MKMapView!
+  
+  var manager = CLLocationManager()
+  
+  var updateCount = 0
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    
+    manager.delegate = self
+    
+    if CLLocationManager.authorizationStatus() == .authorizedWhenInUse {
+      print("Ready to go!")
+      mapView.showsUserLocation = true
+      manager.startUpdatingLocation()
+      
+      Timer.scheduledTimer(withTimeInterval: 5, repeats: true, block: { (timer) in
+        
+        // Spawn a Pokemon
+        
+        print("Spawn")
+        
+        if let coord = self.manager.location?.coordinate {
+          let anno = MKPointAnnotation()
+          anno.coordinate = coord
+          let randoLat = (Double(arc4random_uniform(200)) - 100.0) / 50000.0
+          let randoLon = (Double(arc4random_uniform(200)) - 100.0) / 50000.0
+          anno.coordinate.latitude += randoLat
+          anno.coordinate.longitude += randoLon
+          
+          self.mapView.addAnnotation(anno)
+        }
+      })
+
+    }
+    else {
+      manager.requestWhenInUseAuthorization()
+
+    }
+  }
+  
+  func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+    
+    if updateCount < 3 {
+      
+      let region = MKCoordinateRegionMakeWithDistance((manager.location?.coordinate)!, 200, 200)
+      mapView.setRegion(region, animated: false)
+      updateCount += 1
+    }
+    else {
+      manager.stopUpdatingLocation()
+    }
   }
 
   override func didReceiveMemoryWarning() {
@@ -21,5 +72,12 @@ class ViewController: UIViewController {
   }
 
 
+  @IBAction func centerTapped(_ sender: AnyObject) {
+    
+    if let coord = manager.location?.coordinate {
+    let region = MKCoordinateRegionMakeWithDistance(coord, 200, 200)
+    mapView.setRegion(region, animated: true)
+    }
+  }
 }
 
